58bfea3088d929d2f5e7db8dce8fbf92a0917618
BBIYAC <kiy7833@gmail.com>
2021-10-27 14:49:53 +0900

    Update signupafter page margin

--- src/pages/SignUpAfter.js
+++ src/pages/SignUpAfter.js
@@ -7,8 +7,8 @@ const SignUpAfter = () => {
     return(
         <>
         <div className="title">회원가입</div>
-        <FaBirthdayCake className="img-welcome" style={{color: '#5BB1F9', width: '80px', height: '80px', marginTop: '70%'}}/>
-        <div className="img-welcome" style={{color: '#5BB1F9', margin: '10px 0 100% 0'}}>가입을 환영합니다 :)</div>
+        <FaBirthdayCake className="img-welcome" style={{color: '#5BB1F9', width: '20vw', height: '20vw', marginTop: '20vh'}}/>
+        <div className="img-welcome" style={{color: '#5BB1F9', margin: '10px 0 30vh 0'}}>가입을 환영합니다 :)</div>
         <RoundRectangle btnText="로그인" />
         </>
     );
