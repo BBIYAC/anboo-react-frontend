ac91a718562063ffaf19002b97907bd32f1d0806
BBIYAC <kiy7833@gmail.com>
2021-10-28 15:16:39 +0900

    Add phonenumber in signup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ANBOO! Project
 
-초기 세팅
+초기 세팅 (버전 준수)
 - Node.js 설치
-- npm install react-router-dom --save
-- npm install react-icons --save
+- npm install react-router-dom@5.3.0 --save
+- npm install react-icons@^4.3.1 --save
+- (react-scripts 오류) npm install react-scripts@4.0.3
--- src/components/atoms/Input/PhoneNumber.js
+++ src/components/atoms/Input/PhoneNumber.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PhoneNumber = () =>{
+    const [titPhone, setTitPhone] = useState('');
+    const onChange = (e) => {
+        setTitPhone(e.target.value);
+    };
     return(
         <React.Fragment>
-            
+            <div className="tit-name">전화번호</div>
+            <input className="input-name" onChange={onChange} value={titPhone}></input>
         </React.Fragment>
     );
 };
