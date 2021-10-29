81c0d486f0b120a218f017761e6fbf9422c6c40b
inyeong <kiy7833@gmail.com>
2021-10-30 02:51:09 +0900

    Move manager home before to templates

--- src/pages/ManagerHome.js
+++ src/pages/ManagerHome.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {IoIosArrowBack} from 'react-icons/io';
 import {BiLogOut} from 'react-icons/bi';
-import Search from '../components/atoms/Input/Search';
-import NursingHomeInfoCheckBlock from '../components/molecules/Block/NursingHomeInfoCheckBlock';
 import ManagerBelowBarBlock from '../components/molecules/Block/ManagerBelowBarBlock';
-import RoundRectangle from '../components/atoms/Button/RoundRectangle';
-import AddCertificationFile from '../components/atoms/Input/AddCertificationFile';
 import '../components/atoms/Label/Label.css';
 import '../components/molecules/Block/Block.css';
-import NoticeBlock from '../components/molecules/Block/NoticeBlock';
+import ManagerHomeBefore from '../components/templates/ManagerHome/ManagerHomeBefore';
 
 const ManagerHome = () => {
     return (
@@ -18,12 +14,7 @@ const ManagerHome = () => {
               관리자 홈
               <BiLogOut size="20"/>
             </div>
-            <NoticeBlock />
-            <Search />
-            <NursingHomeInfoCheckBlock isSelected />
-            <NursingHomeInfoCheckBlock />
-            <AddCertificationFile />
-            <RoundRectangle btnText='요양원 등록 요청하기' background='var(--color-green)' />
+            <ManagerHomeBefore />
             <ManagerBelowBarBlock isHome />
         </React.Fragment>
     );
