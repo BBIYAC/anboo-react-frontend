241bc7c1c66c0df279e892b0c5efdf61d47c1f64
Tacksang Kwon <sange17@naver.com>
2021-11-01 21:40:24 +0900

    Remove arrow-down style and rating point 0

--- src/components/atoms/Select/Rating.js
+++ src/components/atoms/Select/Rating.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Rating = () => {
 	const ratingList = 
   ['5.0', '4.5', '4.0', '3.5', '3.0',
-   '2.5', '2.0', '1.5', '1.0', '0.5', '0.0'];
+   '2.5', '2.0', '1.5', '1.0', '0.5'];
   const [rating, ratingSelected] = useState('');
 
   const changeSelect = (e) =>{
