9c02237a1d3d97bd1c77c21ec185353cba89606d
inyeong <kiy7833@gmail.com>
2021-11-17 03:07:09 +0900

    Add user distinction key axios code in manager home

--- src/components/templates/ManagerHome/ManagerHomeBefore.js
+++ src/components/templates/ManagerHome/ManagerHomeBefore.js
@@ -5,22 +5,8 @@ import AddCertificationFile from '../../atoms/Input/AddCertificationFile';
 import NoticeBlock from '../../molecules/Block/NoticeBlock';
 import { FaSearch } from 'react-icons/fa';
 import './ManagerHome.css';
-import axios from "axios";
 
 const ManagerHomeBefore = () => {
-  // ################################아래 코드는 각 화면에 모두 들어갈 코드입니다.################################
-  const headers = {
-    Authorization : 'Bearer ' + localStorage.getItem('accessToken')
-  }
-  const apiUrl =  'http://ec2-54-180-93-130.ap-northeast-2.compute.amazonaws.com'
-  axios({url:`${apiUrl}/authentication/check/`,method : 'get' ,headers:headers})
-  .then(response =>{
-      console.log(response)
-
-  }).catch(error => {
-      console.error(error)
-  })
-  // ################################위의 코드는 각 화면에 모두 들어갈 코드입니다.################################
     return(
         <React.Fragment>
             <NoticeBlock />
@@ -40,15 +26,3 @@ const ManagerHomeBefore = () => {
 
 export default ManagerHomeBefore;
 
-
-/* 
-
-브라우저 개발자 모드 console 창의 Object -> data -> key 값
-
-1 : 미등록 보호자 
-2 : 등록보호자 
-3: 미승인 관리자 
-4: 승인된 관리자 
-no key : 비회원 
-
-*/
--- src/pages/ManagerHome.js
+++ src/pages/ManagerHome.js
@@ -7,10 +7,40 @@ import '../components/molecules/Block/Block.css';
 import ManagerHomeBefore from '../components/templates/ManagerHome/ManagerHomeBefore';
 import ManagerHomeWaiting from '../components/templates/ManagerHome/ManagerHomeWaiting';
 import ManagerHomeAfter from '../components/templates/ManagerHome/ManagerHomeAfter';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from "axios";
 
 const ManagerHome = () => {
+  let history = useHistory();
   const [userState, setUserState] = useState('');
+  
+  // ################################사용자 구분 코드################################
+  const headers = {
+    Authorization : 'Bearer ' + localStorage.getItem('accessToken')
+  }
+  const apiUrl =  'http://ec2-54-180-93-130.ap-northeast-2.compute.amazonaws.com';
+  useEffect(()=>{
+    axios({url:`${apiUrl}/authentication/check/`,method : 'get' ,headers:headers})
+    .then(response =>{
+      let key = response.data.key;
+      if(key === 1){ // 미등록 보호자
+        history.push('/rg/nh-location');
+      }else if(key === 2){ // 등록 보호자
+        history.push('/rg/acts');
+      }else if(key === 3){ // 미승인 관리자
+        setUserState('before');
+      }else if(key === 4){ // 승인 관리자
+        setUserState('after');
+      }else{ // 관리자 승인 대기
+        setUserState('waiting');
+      }
+    }).catch(error => { // 로그인 token 없는 경우(비회원)
+        console.error(error);
+        history.push('/rg/nh-location');
+    })
+  },[])
+  // ################################사용자 구분 코드################################
+
   const pageState = (state) => {
     switch(state){
       case 'after':{
