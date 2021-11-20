b7f35f7d54bfef3362c4c5aa3c991c0c98a6a561
Tacksang Kwon <sange17@naver.com>
2021-11-20 23:44:28 +0900

    Add search function in rg-nhs page Simplify search function in rg-nhs page

--- src/pages/RegisterNhs.js
+++ src/pages/RegisterNhs.js
@@ -4,7 +4,6 @@ import Religion from '../components/atoms/Select/Religion';
 import Favorites from '../components/atoms/Button/Favorites';
 import Floating from '../components/atoms/Button/Floating';
 import NursingHomeInfoBlock from '../components/molecules/Block/NursingHomeInfoBlock';
-import AllNHInfoBlock from '../components/molecules/Block/AllNHInfoBlock';
 import axios from "axios";
 import { FaSearch } from 'react-icons/fa';
 import { Link, useHistory } from 'react-router-dom';
@@ -18,28 +17,36 @@ import '../components/atoms/Label/Label.css';
 const RegisterNhs= () => {
   // ################################사용자 구분 코드################################
   let history = useHistory();
-  let arr = [];
   const [userState, setUserState] = useState('');
   const [logState, setLogState] = useState(false);
   const [nursingHomes, setNursingHomes] = useState([]);
-  const [allNH, setAllNH] = useState([]);
+  const [search, setSearch] = useState("");
   const [headers, setHeaders] = useState({Authorization : 'Bearer ' + localStorage.getItem('accessToken')})
-  useEffect(()=>{
 
+  useEffect(()=>{
     if(headers.Authorization.split(" ")[1] === "null"){
       headers.Authorization = '';
     };
     axios({url:`${apiUrl}/authentication/check/`,method : 'get' ,headers:headers})
     .then(response =>{
       let key = response.data.key;
-      const allList = history.location.state.allList;
       if(key === 1){ // 미등록 보호자
-        console.log(history.location.state.allList);
-        console.log(history.location.state.searchDefault);
-        axios({url:`${apiUrl}/nh-info/search=${history.location.state.cityName}/`, method: 'get'})
-        .then(response=>{
-          setNursingHomes(response.data);
-        })
+        if(search){
+          axios({url:`${apiUrl}/nh-info/search=${search}/`, method: 'get'})
+          .then(response=>{
+            setLogState(true);
+            setNursingHomes(response.data);
+          }).catch(error=> {
+            console.error(error);
+          })
+        }else{
+          axios({url:`${apiUrl}/nh-info/`, method: 'get'})
+          .then(response=>{
+            setNursingHomes(response.data);
+          }).catch(error=> {
+            console.error(error);
+          })
+        }
       }else if(key === 2){ // 등록 보호자
         history.push('/rg/acts');
       }else if(key === 3){ // 미승인 관리자
@@ -47,30 +54,29 @@ const RegisterNhs= () => {
       }else if(key === 4){ // 승인 관리자
         setUserState('after');
       }else if(key === 6){  // 비회원
-        console.log(history.location.state.searchDefault);
-        if(allList){
-          axios({url:`${apiUrl}/nh-info/`, method: 'get'})
-          .then(response=>{
-            console.log("여기");
-            console.log(response.data[0]);
-            setAllNH(response.data);
-            setLogState(true);
-          })
-        }else{
-          axios({url:`${apiUrl}/nh-info/search=${history.location.state.cityName}/`, method: 'get'})
+        if(search){
+          axios({url:`${apiUrl}/nh-info/search=${search}/`, method: 'get'})
           .then(response=>{
             setLogState(true);
             setNursingHomes(response.data);
-            console.log("여기");
+          }).catch(error=> {
+            console.error(error);
+          })
+        }else{
+          axios({url:`${apiUrl}/nh-info/`, method: 'get'})
+          .then(response=>{
+            setLogState(true);
+            setNursingHomes(response.data);
+          }).catch(error=> {
+            console.error(error);
           })
         }
-        
       }else{ // 관리자 승인 대기
         setUserState('waiting');
       }
-    }).catch(error => { // 로그인 token 없는 경우(비회원)
+    }).catch(error => {
     })
-  },[])
+  },[search]);
   // ################################사용자 구분 코드################################
   
   // 로그아웃 이벤트
@@ -82,19 +88,16 @@ const RegisterNhs= () => {
     history.push('/');
   }
 
-  // 마커 클릭된 지역의 요양원
+  // 요양원 렌더링
   const renderNursingHomes = nursingHomes.map(nursingHome => {
     return (
       <NursingHomeInfoBlock nursingHome={nursingHome}/>
     );
   });
 
-  // 모든 요양원
-  const renderAllNH = allNH.map(NH => {
-    return (
-      <AllNHInfoBlock nh={NH}/>
-    );
-  });
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  }
 
   return (
     <React.Fragment>
@@ -109,12 +112,13 @@ const RegisterNhs= () => {
         <div className="block-search">
           <div className="box-search">
             <input 
-            className="searchBox"
-            type="text"
-            // 지도 화면에서 마커를 클릭할 시와 클릭하지 않고 상단의 검색하러 가기 버튼을 누를 시 이벤트를 다르게 하기 위함.
-            placeholder={history.location.state.searchDefault
-              ?"요양원 이름, 주소, 지역 검색하기"
-              :history.location.state.cityName}
+              className="searchBox"
+              type="text"
+              // 지도 화면에서 마커를 클릭할 시와 클릭하지 않고 상단의 검색하러 가기 버튼을 누를 시 이벤트를 다르게 하기 위함.
+              placeholder={history.location.state.searchDefault
+                ? "요양원 이름, 주소 검색하기"
+                : history.location.state.cityName}
+              onChange={onChange}
             >
             </input>
             <button className="search-icon"><FaSearch size="20"/></button>
@@ -128,7 +132,6 @@ const RegisterNhs= () => {
       </div>
       <div className="div-nursingHomeScroll">
         {renderNursingHomes}
-        {renderAllNH}
       </div>
       <Link className="linkComponent" to="/rg/nh-location">
         <Floating background="var(--color-blue)"/>
